f69376975b8e7340a33484f3f8c17520ab19cfa5
agungsaputro <agungsaputro484@gmail.com>
2020-07-29 23:36:06 +0700

    update app.js

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
-const argv = process.argv.slice(2)
+//const argv = process.argv.slice(2)
 
-const controllerTodo = require('./controllers/todo')
+//const controllerTodo = require('./controllers/todo')
 
-const [table, command, ...args] = argv
+//const [table, command, ...args] = argv
+
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize('database', 'username', 'password', {
+    host: 'localhost',
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+      }
+  });
+
+  sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 switch(table){
     case "todo":{
